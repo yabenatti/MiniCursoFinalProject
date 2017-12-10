1e1ebda7d3294c74bad9ab453c9a14339b917921
yabenatti <min.benatti@gmail.com>
2017-12-10 21:29:14 -0200

    Inserts tableview to CoreDataViewController

--- MiniCursoFinalProject/ViewControllers/CoreData/CoreDataViewController.swift
+++ MiniCursoFinalProject/ViewControllers/CoreData/CoreDataViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class CoreDataViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Variables
+    var items: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //Bar Button Item
         self.navigationItem.setRightBarButton(UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItemToList)), animated: true)
-
+        
+        //TableView
+        self.tableView.dataSource = self
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,5 +46,20 @@ class CoreDataViewController: UIViewController {
     func addItemToList(_ sender: UIBarButtonItem) {
         print("add item")
     }
+}
 
+// MARK: - TableView Methods
+
+extension CoreDataViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
 }
